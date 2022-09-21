1ba170d1af0bb601dc51fc618d5b4dd82d1de919
massimoalbarello <massimo00albarello@gmail.com>
2022-09-21 23:13:48 +0200

    append sibling to one of old parents or child to one of new parents

--- src/block_tree.rs
+++ src/block_tree.rs
@@ -1,63 +1,110 @@
 use std::rc::Rc;
 
 pub struct Block {
-    payload: String,
     parent_ref: Option<Rc<Block>>,
+    payload: String,
+    height: u32,
 }
 
 impl Block {
-    fn new(parent_ref: Option<Rc<Block>>, payload: String) -> Self {
+    fn new(parent_ref: Option<Rc<Block>>, payload: String, height: u32) -> Self {
         Self {
-            payload,
             parent_ref,
+            payload,
+            height,
         }
     }
 }
 
 struct BlockTree {
-    tip_ref: Rc<Block>,
+    previous_tips_refs: Vec<Rc<Block>>,
+    tips_refs: Vec<Rc<Block>>,
+    current_height: u32,
 }
 
 impl BlockTree {
     fn new() -> Self {
         Self {
-           tip_ref: Rc::new(Block::new(None, String::from("Genesis"))),
+            previous_tips_refs: vec![Rc::new(Block::new(None, String::from("Genesis"), 0))],
+            tips_refs: vec![],
+            current_height: 0,
         }
     }
 
-    fn create_child(&mut self, parent_ref: Rc<Block>, payload: String) {
-        self.tip_ref = Rc::new(Block::new(Some(parent_ref), payload));
+    fn create_child_at_height(
+        &mut self,
+        child_height: u32,
+        index_in_tips_refs: usize,
+        payload: String,
+    ) {
+        if child_height == self.current_height + 1 {
+            match self.previous_tips_refs.get(index_in_tips_refs) {
+                Some(parent_ref) => {
+                    self.tips_refs.push(Rc::new(Block::new(
+                        Some(Rc::clone(parent_ref)),
+                        payload,
+                        child_height,
+                    )));
+                }
+                None => println!(
+                    "No parent at height {} and width {}",
+                    child_height - 1,
+                    index_in_tips_refs
+                ),
+            }
+        } else if child_height == self.current_height + 2 {
+            match self.tips_refs.get(index_in_tips_refs) {
+                Some(parent_ref) => {
+                    self.previous_tips_refs = self.tips_refs.to_owned();
+                    self.tips_refs = vec![Rc::new(Block::new(
+                        Some(Rc::clone(parent_ref)),
+                        payload,
+                        child_height,
+                    ))];
+                    self.current_height += 1;
+                }
+                None => println!(
+                    "No parent at height {} and tip index {}",
+                    child_height, index_in_tips_refs
+                ),
+            }
+        } else {
+            println!("Invalid child height");
+        }
     }
 
-    fn display_chain_from_tip(&self, mut block: &Rc<Block>) {
-        loop {
-            println!("{}", block.payload);
-            block = match block.parent_ref.as_ref() {
-                Some(parent) => parent,
-                None => break,
-            }
+    fn display_chain_from_tip(&self, index_in_tips_refs: usize) {
+        match self.tips_refs.get(index_in_tips_refs) {
+            Some(mut block) => {
+                loop {
+                    println!("{} at height {}", block.payload, block.height);
+                    block = match block.parent_ref.as_ref() {
+                        Some(parent) => parent,
+                        None => break,
+                    }
+                }
+                println!("");
+            },
+            None => println!("Invalid tip index"),
         }
     }
 }
 
 fn main() {
     let mut block_tree = BlockTree::new();
-    let genesis_ref_a = Rc::clone(&block_tree.tip_ref);
-    let genesis_ref_b = Rc::clone(&block_tree.tip_ref);
 
-    block_tree.create_child(genesis_ref_a, String::from("Block 1a"));
-    let block_1a_ref = Rc::clone(&block_tree.tip_ref);
+    block_tree.create_child_at_height(1, 0, String::from("Block 1_a"));
+    block_tree.create_child_at_height(1, 0, String::from("Block 1_b"));
+    block_tree.create_child_at_height(1, 0, String::from("Block 1_c"));
 
-    block_tree.create_child(genesis_ref_b, String::from("Block 1b"));
-    let block_1b_ref = Rc::clone(&block_tree.tip_ref);
+    block_tree.create_child_at_height(2, 0, String::from("Block 2_a_a"));
+    block_tree.create_child_at_height(2, 0, String::from("Block 2_a_b"));
 
-    block_tree.create_child(block_1a_ref, String::from("Block 2a"));
-    let block_2a_ref = Rc::clone(&block_tree.tip_ref);
+    block_tree.create_child_at_height(2, 1, String::from("Block 2_b_a"));
+    block_tree.create_child_at_height(2, 1, String::from("Block 2_b_b"));
 
-    block_tree.create_child(block_1b_ref, String::from("Block 2b"));
-    let block_2b_ref = Rc::clone(&block_tree.tip_ref);
-
-    block_tree.display_chain_from_tip(&block_2a_ref);
-    block_tree.display_chain_from_tip(&block_2b_ref);
-
+    block_tree.display_chain_from_tip(0);
+    block_tree.display_chain_from_tip(1);
+    block_tree.display_chain_from_tip(2);
+    block_tree.display_chain_from_tip(3);
 }
