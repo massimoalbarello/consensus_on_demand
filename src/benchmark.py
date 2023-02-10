f202110f4c859aece1164bc1d4801b7f7e9946e5
massimoalbarello <massimo00albarello@gmail.com>
2023-02-10 16:45:06 +0100

    distinguish IC from FP bars + FP sequences metrics

--- src/benchmark.py
+++ src/benchmark.py
@@ -30,8 +30,7 @@ def getBenchmarks():
             results.append(json.loads(f.read()))
     return results
 
-def fill_missing_elements(iterations, metrics):
-    range_ = max(iterations) - min(iterations) + 1
+def fillMissingElements(iterations, metrics, default_element):
     filled_iterations = []
     filled_metrics = []
     for i in range(min(iterations), max(iterations) + 1):
@@ -41,30 +40,83 @@ def fill_missing_elements(iterations, metrics):
             filled_metrics.append(metrics[index])
         else:
             filled_iterations.append(i)
-            filled_metrics.append(-1)
+            filled_metrics.append(default_element)
     return filled_iterations, filled_metrics
+
+def countFpSequences(first_index_offset, arr):
+    sequences = []
+
+    sequence_length = None
+    not_finalized_sequence_len = 0
+    starting_index = 0 # genesis block is IC finalized
+    for i in range(len(arr)):
+        if arr[i] == 'IC':
+            # register previous sequence
+            sequences.append({
+                "length": sequence_length,
+                "IC_index": starting_index,
+            })
+            # initialize new sequence
+            starting_index = i+first_index_offset
+            sequence_length = 0
+        elif arr[i] == 'FP':
+            if sequence_length == None: # sequence length can be "None" only for the first sequence
+                sequence_length = i+first_index_offset
+            else:
+                sequence_length += 1
+        elif arr[i] == '-':
+            not_finalized_sequence_len += 1
+            if i < len(arr)-1 and arr[i+1] == 'IC':
+                not_finalized_sequence_len = 0
+            elif i < len(arr)-1 and arr[i+1] == 'FP':
+                sequence_length += not_finalized_sequence_len
+                not_finalized_sequence_len = 0
+
+    # register last sequence
+    sequences.append({
+        "length": sequence_length,
+        "IC_index": starting_index, 
+    })
+
+    return sequences
+
+def printStatistics(i, first_index_offset, latencies, filled_finalization_types):
+    print("\n### Replica", i+1, "###")
+    if len(latencies) != 0:
+        average = sum(latencies) / len(latencies)
+        print("The average time for block finalization is:", average)
+    total_fp_finalizations = filled_finalization_types.count("FP")
+    total_ic_finalizations = filled_finalization_types.count("IC")
+    total_non_finalizations = filled_finalization_types.count("-")
+    print("The number of iterations in which the block is:")
+    print("- FP finalized:", total_fp_finalizations)
+    print("- IC finalized:", total_ic_finalizations)
+    print("- not explicitly finalized:", total_non_finalizations)
+    if COD:
+        sequences = countFpSequences(first_index_offset, filled_finalization_types)
+        print("Found", len(sequences), "sequences:")
+        for sequence in sequences:
+            print("- starting at", sequence["IC_index"], "with length", sequence["length"])
 
 def plotResults():
     plt.figure()
     for i, benchmark in enumerate(benchmarks):
-        x = [int(iteration) for iteration in benchmark["results"].keys()]
-        y = [metrics["latency"]["secs"]+metrics["latency"]["nanos"]*1e-9 for metrics in benchmark["results"].values()]
-        if len(y) != 0:
-            average = sum(y) / len(y)
-            print("The average time for block finalization for replica", i+1, "is:", average)
-        filled_iterations, filled_metrics = fill_missing_elements(x, y)
-        plt.subplot(N, 1, i+1)
-        plt.bar(filled_iterations, filled_metrics, width=1)
-        y = [0.5 if metrics["fp_finalization"] == True else 0 for metrics in benchmark["results"].values()]
-        total_fp_finalizations = y.count(0.5)
-        total_ic_finalizations = y.count(0)
-        print("The number of iterations in which the block was FP finalized:", total_fp_finalizations)
-        print("The number of iterations in which the block was IC finalized:", total_ic_finalizations)
-        _, filled_metrics = fill_missing_elements(x, y)
-        total_non_finalizations = filled_metrics.count(-1)
-        print("The number of iterations in which a block wasn't finalized:", total_non_finalizations)
-        plt.subplot(N, 1, i+1)
-        plt.bar(filled_iterations, filled_metrics, width=1)
+        iterations = [int(iteration) for iteration in benchmark["results"].keys()]
+        latencies = [metrics["latency"]["secs"]+metrics["latency"]["nanos"]*1e-9 for metrics in benchmark["results"].values()]
+        filled_iterations, filled_latencies = fillMissingElements(iterations, latencies, 0)
+        finalization_types = ["FP" if metrics["fp_finalization"] == True else "IC" for metrics in benchmark["results"].values()]
+        _, filled_finalization_types = fill_missing_elements(iterations, finalization_types, "-")
+        printStatistics(i, filled_iterations[0], latencies, filled_finalization_types)
+        ax = plt.subplot(N, 1, i+1)
+        for j, type in enumerate(filled_finalization_types):
+            if type == "FP":
+                ax.bar(filled_iterations[j], filled_latencies[j], width=1, color='green')
+            elif type == "IC":
+                ax.bar(filled_iterations[j], filled_latencies[j], width=1, color="blue" )
+        if i == 0:
+            xlim = ax.get_xlim()
+        else:
+            ax.set_xlim(xlim)
     plt.show()
 
 
@@ -73,7 +125,7 @@ COD = True
 N = 6
 F = 1
 P = 1
-T = 300
+T = 100
 
 print("Runnning " + ("Fast IC Consensus" if COD else "original IC Consensus"))
 
