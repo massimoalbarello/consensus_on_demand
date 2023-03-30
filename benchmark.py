399ebaa96a462954e7647150b47528030995be9b
massimoalbarello <massimo00albarello@gmail.com>
2023-03-30 17:19:53 +0200

    box plots

--- benchmark.py
+++ benchmark.py
@@ -1,106 +1,39 @@
+import os
 import json
+import statistics
 import matplotlib.pyplot as plt
 
-def getBenchmarks():
-    with open('./benchmark/res_16_2_0_3000_60_1680020111/benchmark_results_1.json', 'r') as f:
-        return json.loads(f.read())
+results = [
+    {
+        "folder": "./benchmark/ICC_16_0_0_3000_60_1680033093",
+        "label": "ICC n=16 f=0"
+    },
+    {
+        "folder": "./benchmark/res_16_2_0_3000_60_1680020111",
+        "label": "FICC n=16 f=2 p=0"
+    },
+    {
+        "folder": "./benchmark/res_16_0_0_3000_300_1680028847",
+        "label": "FICC n=16 f=p=0"
+    },
+]
 
-def fillMissingElements(iterations, metrics, default_element):
-    filled_iterations = []
-    filled_metrics = []
-    for i in range(min(iterations), max(iterations) + 1):
-        if i in iterations:
-            index = iterations.index(i)
-            filled_iterations.append(iterations[index])
-            filled_metrics.append(metrics[index])
-        else:
-            filled_iterations.append(i)
-            filled_metrics.append(default_element)
-    return filled_iterations, filled_metrics
+box_plots = []
+labels = []
 
-def printMetrics(
-    average_latency,
-    total_fp_finalizations,
-    total_ic_finalizations,
-    total_dk_finalizations,
-    total_non_finalizations,
-):
-    print("The average time for block finalization is:", average_latency)
-    print("The number of iterations in which the block is:")
-    print("- FP finalized:", total_fp_finalizations)
-    print("- IC finalized:", total_ic_finalizations)
-    print("- DK finalized:", total_dk_finalizations)
-    print("- not explicitly finalized:", total_non_finalizations)
+for res in results:
+    subnet_latencies = []
+    for filename in os.listdir(res["folder"]):
+        if filename.endswith(".json"):
+            with open(os.path.join(res["folder"], filename)) as f:
+                data = json.load(f)
+            # extract the latency values from the data
+            subnet_latencies.extend([value["latency"]["secs"] + value["latency"]["nanos"] / 1000000000 for value in data['finalization_times'].values()])
+    box_plots.append(subnet_latencies)
+    labels.append(res["label"])
 
-def processResults(latencies, filled_iterations, filled_finalization_types):
-    average_latency = None
-    if len(latencies) != 0:
-        average_latency = sum(latencies) / len(latencies)
-    total_fp_finalizations = filled_finalization_types.count("FP")
-    total_ic_finalizations = filled_finalization_types.count("IC")
-    total_dk_finalizations = filled_finalization_types.count("DK")
-    total_non_finalizations = filled_finalization_types.count("-")
-
-    return (
-        average_latency,
-        total_fp_finalizations,
-        total_ic_finalizations,
-        total_dk_finalizations,
-        total_non_finalizations,
-    )
-
-def plotLatencies(ax, filled_iterations, filled_latencies, filled_finalization_types):
-    colors = ["green", "blue", "grey"]
-    color_labels = {
-        "green": "FP-finalized block",
-        "blue": "IC-finalized block",
-        "grey": "finalization from peer"
-    }
-    fp_bar = None
-    ic_bar = None
-    for j, type in enumerate(filled_finalization_types):
-        if type == "FP":
-            fp_bar = ax.bar(filled_iterations[j], filled_latencies[j], width=1, color=colors[0], label=color_labels[colors[0]])
-        elif type == "IC":
-            ic_bar = ax.bar(filled_iterations[j], filled_latencies[j], width=1, color=colors[1], label=color_labels[colors[1]])
-        elif type == "DK":
-            ic_bar = ax.bar(filled_iterations[j], filled_latencies[j], width=1, color=colors[2], label=color_labels[colors[2]])
-    handles = [fp_bar, ic_bar]
-    labels = ["FP-finalized block", "IC-finalized block", "finalization from peer"]
-    ax.legend(handles, labels, loc="upper right")
-
-def getResults():
-    plt.plot() 
-    iterations = [int(iteration) for iteration in benchmark["finalization_times"].keys()]
-    latencies = [metrics["latency"]["secs"]+metrics["latency"]["nanos"]*1e-9 for metrics in benchmark["finalization_times"].values()]
-    filled_iterations, filled_latencies = fillMissingElements(iterations, latencies, 0)
-    finalization_types = [metrics["fp_finalization"] for metrics in benchmark["finalization_times"].values()]
-    _, filled_finalization_types = fillMissingElements(iterations, finalization_types, "-")
-
-    (
-        average_latency,
-        total_fp_finalizations,
-        total_ic_finalizations,
-        total_dk_finalizations,
-        total_non_finalizations,
-    ) = processResults(latencies, filled_iterations, filled_finalization_types)
-
-    ax = plt.gca()
-    ax.set_xlabel("Round")
-    ax.set_ylabel("Latency [s]")
-    plotLatencies(plt.gca(), filled_iterations, filled_latencies, filled_finalization_types)
-    plt.show()
-
-    printMetrics(
-        average_latency,
-        total_fp_finalizations,
-        total_ic_finalizations,
-        total_dk_finalizations,
-        total_non_finalizations,
-    )
-
-
-
-benchmark = getBenchmarks()
-
-getResults()
+# create the boxplot
+plt.boxplot(box_plots, labels=labels, showfliers=True, flierprops=dict(marker='o', markerfacecolor='gray', markersize=4, linestyle='none', markeredgecolor='gray'))
+plt.title('Latency Distribution')
+plt.ylabel('Latency (seconds)')
+plt.show()
